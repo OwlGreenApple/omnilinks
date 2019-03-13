ad8429f19d6268aaebc89f78102ef35c838ecca4
ilham firnanda <fifir.celebgramme@gmail.com>
2019-03-13 11:17:55 +0700

    project

---
+++ resources/views/user/link/link.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html  lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/linkmain.css')}}">
+	<title>Link</title>
+	<body class="default-theme">
+	<div class="container biolink-page">
+	<header></header>
+	<ul class="links messengers links-num-6">
+	<li class="link"><a href="/prPx" title="Email" class="btn"><i class="fas fa-envelope"></i></a></li>
+	<li class="link"><a href="/ABpr" title="Skype" class="btn"><i class="fab fa-skype"></i></a></li>
+
+	<li class="link"><a href="/Ohlr" title="Telegram" class="btn"><i class="fab fa-telegram-plane"></i></a></li>
+	<li class="link"><a href="/s9DA" title="WhatsApp" class="btn"><i class="fab fa-whatsapp"></i></a></li>
+
+	<li class="link"><a href="/u605" title="Messenger" class="btn"><i class="fab fa-facebook-messenger"></i></a></li>
+	</ul>
+
+	<ul class="links buttons">
+		
+		<li class="link"><a href="/mUOz" title="awesawe" class="btn"><span>awesawe</span></a></li>
+		<li class="link"><a href="/9m7X" title="asdsasw" class="btn"><span>asdsasw</span></a></li>
+		<li class="link"><a href="/i0Ly" title="jhkjhkjhkjh" class="btn"><span>jhkjhkjhkjh</span></a></li>
+	</ul>
+	<ul class="links social_links links-num-7">
+		<li class="link"><a href="/EdT0" title="Twitter"></a><i class="fab fa-twitter"></i></li>
+		<li class="link"><a href="/5V3m" title="fb"><i class="fab fa-facebook-f"></i></a></li>
+		<li class="link"><a href="/4rCk" title="Youtube"><i class="fab fa-facebook-f"></i></a></li>
+		<li class="link"><a href="/9FhK" title="ig"><i class="fab fa-instagram"></i></a></li>
+	</ul>
+</div>
+<div class="powered-by-shorby"><a href="">powered by&nbsp;&nbsp;<span class="shorby-logo">Omnilinks</span>
+</a>
+</div>
+</body>
+</head>
+</html>
