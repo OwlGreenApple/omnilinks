f1bb0f1677b2baa45f3ec58955a802631c0fe194
rizky redjo <celebgramme.dev@gmail.com>
2019-05-23 21:44:04 +0700

    shortlink purpose

--- routes/web.php
+++ routes/web.php
@@ -9,46 +9,48 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+  DOMAIN_TYPE
+  APP_URL
+  SHORT_LINK
 */
+if(env('DOMAIN_TYPE')=='main'){
+  Route::get('/', 'HomeController@index');
+  // Route::get('/use','UserController@index');
+  // Route::post('/user/create','UserController@create');
+  // Route::get('/use/{id}/edit','UserController@edit');
+  // Route::post('/use/{id}/update','UserController@update');
+  // Route::get('/use/{id}/delete','UserController@delete');
+  Auth::routes();
 
-Route::get('/', 'HomeController@index');
-// Route::get('/use','UserController@index');
-// Route::post('/user/create','UserController@create');
-// Route::get('/use/{id}/edit','UserController@edit');
-// Route::post('/use/{id}/update','UserController@update');
-// Route::get('/use/{id}/delete','UserController@delete');
-Auth::routes();
+  //redirect
+  Route::get('click/{mode}/{id}', 'BiolinkController@click');
 
-//redirect
-Route::get('click/{mode}/{id}', 'BiolinkController@click');
-
-//information
-    Route::get('/about',function(){
+  //information
+  Route::get('/about',function(){
     return view('aboutus');
-    });
-    Route::get('/faq',function(){
+  });
+  Route::get('/faq',function(){
     return view('faq');
-    });
-    Route::get('/helps',function(){
+  });
+  Route::get('/helps',function(){
     return view('term and policy');
-    });
-//auth
-    Route::post('post-register', 'Auth\RegisterController@post_register');
-    Route::get('/verifyemail/{cryptedcode}','Auth\LoginController@verifyemail');
-    Route::get('/home', 'HomeController@index')->name('home');
+  });
+  //auth
+  Route::post('post-register', 'Auth\RegisterController@post_register');
+  Route::get('/verifyemail/{cryptedcode}','Auth\LoginController@verifyemail');
+  Route::get('/home', 'HomeController@index')->name('home');
 
-    //pricing
-    Route::get('/pricing',function(){
-      return view('pricing.pricing');
-    });
-    Route::get('/checkout/{id}','OrderController@checkout');
-    Route::post('/check-kupon','OrderController@check_kupon');
-    Route::post('/register-payment','OrderController@register');
-    Route::post('/confirm-payment','OrderController@confirm_payment');
-    Route::get('/thankyou','OrderController@thankyou');
+  //pricing
+  Route::get('/pricing',function(){
+    return view('pricing.pricing');
+  });
+  Route::get('/checkout/{id}','OrderController@checkout');
+  Route::post('/check-kupon','OrderController@check_kupon');
+  Route::post('/register-payment','OrderController@register');
+  Route::post('/confirm-payment','OrderController@confirm_payment');
+  Route::get('/thankyou','OrderController@thankyou');
 
-
-    Route::group(['middleware' => ['web','auth']], function () {
+  Route::group(['middleware' => ['web','auth']], function () {
     Route::get('/orders','OrderController@index_order');
     Route::get('/orders/load-order','OrderController@load_order');
     Route::post('/orders/confirm-payment','OrderController@confirm_payment_order');
@@ -96,13 +98,10 @@ Route::get('click/{mode}/{id}', 'BiolinkController@click');
     Route::get('/pixel/deletesinglepixel','SinglelinkController@deletesinglepixel');
     Route::get('/pixel/loadPixelLink','SinglelinkController@loadPixelLink');
     Route::get('/link/deletesinglelink','SinglelinkController@deletesinglelink');
-     //url
-    Route::get('/dash/new/omn.lkz/{names}','BiolinkController@link');
-    Route::get('/omn.lkz/{names}','BiolinkController@link');
-});
+  });
 
 
-Route::group(['middleware'=>['web','auth','thisadmin']],function(){
+  Route::group(['middleware'=>['web','auth','thisadmin']],function(){
     //admin order
     Route::get('/list-order',function(){
       return view('admin.list-order.index');
@@ -122,4 +121,11 @@ Route::group(['middleware'=>['web','auth','thisadmin']],function(){
     Route::get('/list-user/load-user','UserController@load_user');  
     Route::get('/list-user/add-user','UserController@add_user');
     Route::get('/list-user/edit-user','UserController@edit_user');
-});
+  });
+}
+
+if((env('DOMAIN_TYPE')=='shortlink')||(env('APP_ENV')=='local')){
+  //url
+  Route::get('/dash/new/omn.lkz/{names}','BiolinkController@link');
+  Route::get('/omn.lkz/{names}','BiolinkController@link');
+}
