6b961d4a67c578e4ba9e48f5c8dd6d1e5a4799f9
rizky redjo <celebgramme.dev@gmail.com>
2019-06-21 14:36:54 +0700

    fixing snappy

--- config/snappy.php
+++ config/snappy.php
@@ -1,18 +1,24 @@
 <?php
+if (env('APP_ENV')=='local') {
+  $binary = base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf');
+} else {
+  $binary = '/usr/local/bin/wkhtmltopdf';
+}
 
 return array(
 
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary'  => $binary,
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => base_path('vendor\wemersonjanuario\wkhtmltoimage-windows\bin\64bit\wkhtmltoimage'),
+        // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltoimage-windows\bin\64bit\wkhtmltoimage'),
+        'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
