8d1a1891cd31749fa69a51c388f3d41f0b91b2b3
PuspitaN <puspita.celebgramme@gmail.com>
2019-06-25 09:05:46 +0700

    bug pdf

--- config/snappy.php
+++ config/snappy.php
@@ -2,7 +2,7 @@
 if (env('APP_ENV')=='local') {
   $binary = base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf');
 } else {
-  $binary = '/usr/local/bin/wkhtmltopdf';
+  $binary = '/usr/local/bin/wkhtmltopdf-amd64';
   //$binary = base_path('vendor\rvanlaak\wkhtmltopdf-amd64-centos7\bin\wkhtmltopdf-amd64');
 }
 
@@ -19,7 +19,7 @@ return array(
     'image' => array(
         'enabled' => true,
         // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltoimage-windows\bin\64bit\wkhtmltoimage'),
-        'binary'  => '/usr/local/bin/wkhtmltoimage',
+        'binary'  => '/usr/local/bin/wkhtmltoimage-amd64',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
