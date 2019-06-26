5001463d1509adacc0bdffab46deb57e7e01e223
rizky redjo <celebgramme.dev@gmail.com>
2019-06-26 15:45:51 +0700

    Fixing

--- config/snappy.php
+++ config/snappy.php
@@ -2,8 +2,8 @@
 if (env('APP_ENV')=='local') {
   $binary = base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf');
 } else {
-  $binary = base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
-  // $binary = '/usr/local/bin/wkhtmltopdf';
+  // $binary = base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
+  $binary = '/usr/local/bin/wkhtmltopdf';
   //$binary = base_path('vendor\rvanlaak\wkhtmltopdf-amd64-centos7\bin\wkhtmltopdf-amd64');
 }
 
@@ -20,7 +20,7 @@ return array(
     'image' => array(
         'enabled' => true,
         // 'binary'  => base_path('vendor\wemersonjanuario\wkhtmltoimage-windows\bin\64bit\wkhtmltoimage'),
-        'binary'  => '/usr/local/bin/wkhtmltoimage-amd64',
+        'binary'  => '/usr/local/bin/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
