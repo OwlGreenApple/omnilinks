280419f1ad2b3eb1f4c7db7d93d6af56e022e022
rizky redjo <celebgramme.dev@gmail.com>
2019-06-26 14:47:19 +0700

    fixing PDF

--- config/snappy.php
+++ config/snappy.php
@@ -2,7 +2,7 @@
 if (env('APP_ENV')=='local') {
   $binary = base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf');
 } else {
-  $binary = base_path('vendor\h4cc\wkhtmltopdf-amd64\bin\wkhtmltopdf-amd64');
+  $binary = base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
   // $binary = '/usr/local/bin/wkhtmltopdf';
   //$binary = base_path('vendor\rvanlaak\wkhtmltopdf-amd64-centos7\bin\wkhtmltopdf-amd64');
 }
