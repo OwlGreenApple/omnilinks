b1c09be13baf6c7b501844b5a8702a6e6eb511bb
rizky redjo <celebgramme.dev@gmail.com>
2019-10-10 13:53:38 +0700

    fixing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -605,7 +605,7 @@ class DashboardController extends Controller
         
       $first_date = date('d-m-Y',strtotime('+1 day', strtotime($first_date)));
     }*/
-    dd($arr);
+    // dd($arr);
     return $arr;
   }
 
