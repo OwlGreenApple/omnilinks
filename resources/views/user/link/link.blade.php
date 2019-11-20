56362a5f0cfae90d27cc9c9804cf234f7ffd09ca
rizky redjo <celebgramme.dev@gmail.com>
2019-11-20 17:11:08 +0700

    fixing

--- resources/views/user/link/link.blade.php
+++ resources/views/user/link/link.blade.php
@@ -1467,7 +1467,7 @@ and add more";
       </ul>
       <div class="col-lg-7 col-md-8 mb-5 text-center powered-omnilinks">
         @if($pages->powered==1)
-          <a href="#" data-href="https://omnilinkz.com"><!--powered by<br>Omnilinkz-->
+          <a href="#" href="https://omnilinkz.com"><!--powered by<br>Omnilinkz-->
           <img style="width: 150px; margin-bottom: 50px;" src="{{asset('image/powered-by.png')}}"></a>
         @endif
       </div>
@@ -1475,7 +1475,7 @@ and add more";
       @if($membership=='free')
         @if(!is_null($ads))
           <div class="col-lg-7 col-md-8 text-center redirect-ads big">
-            <a href="#" data-href="<?php echo env('APP_URL').'/click-ads/'.$ads->id ?>" target="_blank">
+            <a href="<?php echo env('APP_URL').'/click-ads/'.$ads->id ?>" target="_blank">
               <span href="#" class="headline-1-view-get headads">
                   {{$ads->headline}}  
               </span>
