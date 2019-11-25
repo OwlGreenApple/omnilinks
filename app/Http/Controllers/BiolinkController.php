a8ca7566f90eed62e43a1b26eea0f0ca6d22b958
rizky redjo <celebgramme.dev@gmail.com>
2019-11-25 16:50:48 +0700

    fix

--- app/Http/Controllers/BiolinkController.php
+++ app/Http/Controllers/BiolinkController.php
@@ -31,7 +31,7 @@ class BiolinkController extends Controller
     }
 
     $temp_arr = array();
-    $temp_arr['nomorwa'] = ['required', 'string',  'max:191' ];
+    $temp_arr['nomorwa'] = ['required', 'number',  'max:191' ];
     $temp_arr['pesan'] = ['required', 'string',  'max:191' ];
     $temp_arr['textlink'] = ['required', 'string',  'max:191' ];
 
