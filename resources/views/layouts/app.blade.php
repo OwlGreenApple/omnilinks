ecc124f125402e5d4e58de43fc0979aeb8040c03
rizky redjo <celebgramme.dev@gmail.com>
2019-12-05 09:46:33 +0700

    ganti tawk.to

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -335,7 +335,19 @@
   </div> 
 @if (Auth::check()) 
   @if((Auth::user()->membership=='basic') OR (Auth::user()->membership=='elite'))
-    <script type='text/javascript' data-cfasync='false'>window.purechatApi = { l: [], t: [], on: function () { this.l.push(arguments); } }; (function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({c: 'cfe946cc-282f-46a1-b3d5-80e3a76e166d', f: true }); done = true; } }; })();</script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5de86f2d43be710e1d209f28/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->    
   @endif
 @endif
 
