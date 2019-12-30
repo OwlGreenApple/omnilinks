4f897d2937ec6cfb2a4ab4008854df40bfbe4224
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2019-12-30 15:25:33 +0700

    fix-btn-bio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,7 +195,7 @@
                   </li>
                   -->
                   <li class="nav-item">
-                    <a class="nav-link navlog btn btn-bio" href="{{url('pricing')}}">
+                    <a class="nav-link btn btn-bio" href="{{url('pricing')}}">
                       {{ __('Buat Link Anda') }}
                     </a>
                   </li>
