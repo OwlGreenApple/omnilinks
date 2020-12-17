10144348ca3c36c99b9560af3f1ff4fe9030a1e6
Gunardi Omnifluencer <gunardi.omnifluencer@gmail.com>
2020-12-17 10:52:49 +0700

    fix-valid-until

--- app/Http/Controllers/BiolinkController.php
+++ app/Http/Controllers/BiolinkController.php
@@ -514,8 +514,6 @@ class BiolinkController extends Controller
 
       $proof = $this->getProof($page->id);
 
-      dd($user->valid_until);
-
       return view('user.link.link')
               ->with('pages',$page)
               ->with('membership',$user->membership)
