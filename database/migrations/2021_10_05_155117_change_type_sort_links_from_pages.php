99192644ebce4d76427b436253c01fbb9c5d2007
gunomnifluencer <gunardi.omnifluencer@gmail.com>
2021-10-05 08:53:48 +0000

    change-field-sort-link-to-text-from-table-pages---migrate

---
+++ database/migrations/2021_10_05_155117_change_type_sort_links_from_pages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeTypeSortLinksFromPages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->text('sort_link')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+        });
+    }
+}
